690b812985d2ba2c4877fbcac61e16a37b541749
yaboy58 <kenoh.sama@gmail.com>
2015-09-26 22:19:27 -0500

    Wrote first unit test for the ValidationService

--- Services/ValidationService.cs
+++ Services/ValidationService.cs
@@ -10,15 +10,26 @@ namespace AviSynthBatchScriptCreator.Services
     public class ValidationService : IValidationService
     {
         private AVSBatchSettings _avsBatchSettings;
+        private List<Error> _errors;
 
         public ValidationService(AVSBatchSettings avsBatchSettings)
         {
             _avsBatchSettings = avsBatchSettings;
+            _errors = new List<Error>();
         }
 
         public List<Error> Validate()
         {
-            throw new NotImplementedException();
+            BatchDirectoryIsNotEmpty();
+            return _errors;
+        }
+
+        private void BatchDirectoryIsNotEmpty() 
+        {
+            if (_avsBatchSettings.BatchDirectory == string.Empty)
+            {
+                _errors.Add(new Error() { Id = 0, Description = "Batch Directory is required!"});
+            }
         }
     }
 }
