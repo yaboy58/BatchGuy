26718ad64ea18c2ad755c19cd2a79198dc7cb24f
yaboy58 <kenoh.sama@gmail.com>
2015-09-26 22:01:48 -0500

    Forgot model.

---
+++ Models/AVSBatchSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviSynthBatchScriptCreator.Models
+{
+    public class AVSBatchSettings
+    {
+        public string BatchDirectory { get; set; }
+        public string NamingConvention { get; set; }
+        public int NumberOfFiles { get; set; }
+    }
+}
