201ea2d422e407c05b1140e1a3338a0b5b970a15
yaboy58 <kenoh.sama@gmail.com>
2015-11-07 16:21:10 -0600

    *Added new service to call cli processes *Added unit tests to validate the start process info information *Created new model to hold start process info information.

---
+++ src/BatchGuy.App/Parser/Services/CommandLineProcessService.cs
@@ -0,0 +1,46 @@
+﻿using BatchGuy.App.Models;
+using BatchGuy.App.Parser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchGuy.App.Parser.Services
+{
+    public class CommandLineProcessService : ICommandLineProcessService
+    {
+        private CommandLineProcessStartInfo _commandLineProcessStartInfo;
+        private readonly List<Error> _errors;
+
+        public CommandLineProcessService(CommandLineProcessStartInfo commandLineProcessStartInfo)
+        {
+            _commandLineProcessStartInfo = commandLineProcessStartInfo;
+            _errors = new List<Error>();
+        }
+
+        public List<Error> CheckErrors()
+        {
+            this.CheckFileName();
+            this.CheckArguments();
+
+            return _errors;
+        }
+
+        private void CheckFileName()
+        {
+            if (_commandLineProcessStartInfo.FileName == string.Empty || _commandLineProcessStartInfo.FileName == null)
+            {
+                _errors.Add(new Error() { Description = "Invalid file name" });
+            }
+        }
+
+        private void CheckArguments()
+        {
+            if (_commandLineProcessStartInfo.Arguments == string.Empty || _commandLineProcessStartInfo.Arguments == null)
+            {
+                _errors.Add(new Error() { Description = "Invalid arguments" });
+            }
+        }
+    }
+}
---
+++ src/BatchGuy.App/Parser/Services/ICommandLineProcessService.cs
@@ -0,0 +1,14 @@
+﻿using BatchGuy.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchGuy.App.Parser.Services
+{
+    public interface ICommandLineProcessService
+    {
+        List<Error> CheckErrors();
+    }
+}
