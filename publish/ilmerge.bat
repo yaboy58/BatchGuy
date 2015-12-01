5df476c08cce66fd0fa58b4b15089a92f206af58
yaboy58 <kenoh.sama@gmail.com>
2015-12-01 16:21:30 -0600

    *Updated ilmerge.bat

--- publish/ilmerge.bat
+++ publish/ilmerge.bat
@@ -1,3 +1,3 @@
-SET version="v1.9"
+SET version="v1.9.1"
 
 "..\packages\ilmerge.2.14.1208\tools\ILMerge.exe" "..\src\BatchGuy.App\bin\Release\BatchGuy.App.exe" "..\packages\System.Linq.Dynamic.1.0.4\lib\net40\System.Linq.Dynamic.dll" "..\packages\log4net.2.0.4\lib\net45-full\log4net.dll" /out:"BatchGuy-%version%-win64.exe"  /target:winexe /targetplatform:v4 /ndebug
