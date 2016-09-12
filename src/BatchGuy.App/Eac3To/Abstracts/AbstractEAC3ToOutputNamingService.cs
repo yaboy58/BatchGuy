b882c4b249ad86f5d93bff05f8eb77b9d48835e5
yaboy58 <kenoh.sama@gmail.com>
2016-09-12 10:50:10 -0500

    *Updated todo.txt *Created new abstract class for naming conventions *EncodeTemplate1EAC3ToOutputNamingService.cs created for encoding naming conventions

---
+++ src/BatchGuy.App/Eac3To/Abstracts/AbstractEAC3ToOutputNamingService.cs
@@ -0,0 +1,48 @@
+﻿using BatchGuy.App.Eac3to.Models;
+using BatchGuy.App.Extensions;
+using BatchGuy.App.Parser.Models;
+using BatchGuy.App.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchGuy.App.Eac3To.Abstracts
+{
+    public abstract class AbstractEAC3ToOutputNamingService
+    {
+        protected IAudioService _audioService;
+        public AbstractEAC3ToOutputNamingService(IAudioService audioService)
+        {
+            audioService = _audioService;
+        }
+        protected string GetAudioCommentary(BluRayTitleAudio audio)
+        {
+            string commentary = string.Empty;
+            if (audio.IsCommentary)
+                commentary = "-commentary";
+            return commentary;
+        }
+        protected string GetSubtitleCommentary(BluRayTitleSubtitle subtitle)
+        {
+            string commentary = string.Empty;
+            if (subtitle.IsCommentary)
+                commentary = "-commentary";
+            return commentary;
+        }
+        protected string AddWordSeparator(EAC3ToConfiguration eac3toConfiguration, string episodeName)
+        {
+            if (eac3toConfiguration.IsExtractForRemux && eac3toConfiguration.RemuxFileNameTemplate.UsePeriodsInFileName)
+                return episodeName.ReplaceSpacesWithPeriods();
+            else
+                return episodeName;
+        }
+
+        public abstract string GetChapterName(EAC3ToConfiguration eac3toConfiguration, string filesOutputPath, string paddedEpisodeNumber, string episodeName);
+        public abstract string GetVideoName(EAC3ToConfiguration eac3toConfiguration, string filesOutputPath, string paddedEpisodeNumber, string episodeName);
+        public abstract string GetAudioName(EAC3ToConfiguration eac3toConfiguration, BluRayTitleAudio audio, string filesOutputPath, string paddedEpisodeNumber, string episodeName);
+        public abstract string GetSubtitleName(EAC3ToConfiguration eac3toConfiguration, BluRayTitleSubtitle subtitle, string filesOutputPath, string paddedEpisodeNumber, string episodeName);
+        public abstract string GetLogName(EAC3ToConfiguration eac3toConfiguration, string filesOutputPath, string paddedEpisodeNumber, string episodeName);
+    }
+}
---
+++ src/BatchGuy.App/Eac3To/Services/EncodeTemplate1EAC3ToOutputNamingService.cs
@@ -0,0 +1,71 @@
+﻿using BatchGuy.App.Eac3To.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BatchGuy.App.Eac3to.Models;
+using BatchGuy.App.Parser.Models;
+using BatchGuy.App.Shared.Interfaces;
+using BatchGuy.App.Extensions;
+
+namespace BatchGuy.App.Eac3To.Services
+{
+    public class EncodeTemplate1EAC3ToOutputNamingService : AbstractEAC3ToOutputNamingService
+    {
+        public EncodeTemplate1EAC3ToOutputNamingService(IAudioService audioService) : base(audioService)
+        {
+        }
+
+        public override string GetAudioName(EAC3ToConfiguration eac3toConfiguration, BluRayTitleAudio audio, string filesOutputPath, string paddedEpisodeNumber, string episodeName)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (eac3toConfiguration.IsExtractForRemux != true)
+            {
+                sb.Append(string.Format("\"{0}\\{1}{2}-{3}{4}.{5}\"", filesOutputPath, audio.Language, paddedEpisodeNumber, audio.Id.RemoveColons(), this.GetAudioCommentary(audio),
+                _audioService.GetAudioExtension(audio.AudioType)));
+            }
+            return sb.ToString();
+        }
+
+        public override string GetChapterName(EAC3ToConfiguration eac3toConfiguration, string filesOutputPath, string paddedEpisodeNumber, string episodeName)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (eac3toConfiguration.IsExtractForRemux != true)
+            {
+                sb.Append(string.Format("\"{0}\\chapters{1}.txt\"", filesOutputPath, paddedEpisodeNumber));
+            }
+            return sb.ToString();
+        }
+
+        public override string GetLogName(EAC3ToConfiguration eac3toConfiguration, string filesOutputPath, string paddedEpisodeNumber, string episodeName)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (eac3toConfiguration.IsExtractForRemux != true)
+            {
+                sb.Append(string.Format(" -log=\"{0}\\log{1}.log\"", filesOutputPath, paddedEpisodeNumber));
+            }
+            return sb.ToString();
+        }
+
+        public override string GetSubtitleName(EAC3ToConfiguration eac3toConfiguration, BluRayTitleSubtitle subtitle, string filesOutputPath, string paddedEpisodeNumber, string episodeName)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (eac3toConfiguration.IsExtractForRemux != true)
+            {
+                sb.Append(string.Format("\"{0}\\{1}{2}-{3}{4}.sup\"", filesOutputPath, subtitle.Language, paddedEpisodeNumber, subtitle.Id.RemoveColons(), this.GetSubtitleCommentary(subtitle)));
+            }
+            return sb.ToString();
+        }
+
+        public override string GetVideoName(EAC3ToConfiguration eac3toConfiguration, string filesOutputPath, string paddedEpisodeNumber, string episodeName)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (eac3toConfiguration.IsExtractForRemux != true)
+            {
+                sb.Append(string.Format("\"{0}\\video{1}.mkv\"", filesOutputPath, paddedEpisodeNumber));
+            }
+            return sb.ToString();
+        }
+    }
+}
