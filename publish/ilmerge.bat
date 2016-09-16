d898895875a593be7f508827b552bff148de1053
yaboy58 <kenoh.sama@gmail.com>
2016-09-16 10:20:48 -0500

    *Updated todo.txt *ilmerge.bat version updated *AssemblyInfo.cs updated

--- publish/ilmerge.bat
+++ publish/ilmerge.bat
@@ -1,3 +1,3 @@
-SET version="v4.0.0"
+SET version="v4.1.0"
 
 "..\src\packages\ilmerge.2.14.1208\tools\ILMerge.exe" "..\src\BatchGuy.App\bin\Release\BatchGuy.App.exe" "..\src\packages\System.Linq.Dynamic.1.0.4\lib\net40\System.Linq.Dynamic.dll" "..\src\packages\log4net.2.0.4\lib\net45-full\log4net.dll" "..\src\packages\Newtonsoft.Json.7.0.1\\lib\net45\Newtonsoft.Json.dll" /out:"BatchGuy-%version%-win64.exe"  /target:winexe /targetplatform:v4 /ndebug
