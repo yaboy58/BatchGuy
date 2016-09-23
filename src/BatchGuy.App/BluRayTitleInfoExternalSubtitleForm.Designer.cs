b3dc217b01c6175638a85224a3d90146f0aca6ee
yaboy58 <kenoh.sama@gmail.com>
2016-09-23 11:54:11 -0500

    *Updated todo.txt *External Subtitles Screen cannot be resized

--- src/BatchGuy.App/BluRayTitleInfoExternalSubtitleForm.Designer.cs
+++ src/BatchGuy.App/BluRayTitleInfoExternalSubtitleForm.Designer.cs
@@ -165,6 +165,7 @@
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.gbExternalSubtitles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "BluRayTitleInfoExternalSubtitleForm";
