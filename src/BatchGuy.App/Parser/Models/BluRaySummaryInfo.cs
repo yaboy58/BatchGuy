34cd61d999e75892dcc37dddc1a8c832fbd0c170
yaboy58 <kenoh.sama@gmail.com>
2016-09-26 18:48:17 -0500

    *Updated todo.txt *Added Property RemuxFileNameForMovieTemplate to BluRaySummaryInfo

--- src/BatchGuy.App/Parser/Models/BluRaySummaryInfo.cs
+++ src/BatchGuy.App/Parser/Models/BluRaySummaryInfo.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BatchGuy.App.Extensions;
+using BatchGuy.App.Eac3To.Models;
+
 namespace BatchGuy.App.Parser.Models
 {
     public class BluRaySummaryInfo
     {
+        public BluRaySummaryInfo()
+        {
+            RemuxFileNameForMovieTemplate = new EAC3ToRemuxFileNameTemplate();
+        }
+
         public string Eac3ToId { get; set; }
         public string HeaderText { get; set; }
         public string DetailText { get; set; }
@@ -23,5 +30,6 @@ namespace BatchGuy.App.Parser.Models
                     return this.BluRayTitleInfo.EpisodeNumber.StringToNullInt();
             }
         }
+        public EAC3ToRemuxFileNameTemplate RemuxFileNameForMovieTemplate { get; set; }
     }
 }
